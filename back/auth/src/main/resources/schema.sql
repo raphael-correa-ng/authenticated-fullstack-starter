9311116997965e9c2998d7bcf6fe6f56e86fcdba
Raphael Correa <julio123dog@gmail.com>
2024-07-18 22:16:39 -0300

    don't drop tables on start up

--- back/auth/src/main/resources/schema.sql
+++ back/auth/src/main/resources/schema.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS user_credentials;
-
-CREATE TABLE user_credentials (
+CREATE TABLE IF NOT EXISTS user_credentials (
   username    VARCHAR(32) PRIMARY KEY,
   password    VARCHAR(128) NOT NULL,
   authority   INTEGER NOT NULL
